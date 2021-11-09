e797bff62764639517d0af706e8edfd27c180e58
Muhammad Waqar <muhammad.waqar@kryptomind.com>
2021-11-09 07:43:50 +0500

    browser intent done

--- app/src/main/java/com/example/bsef18a521/MainActivity.java
+++ app/src/main/java/com/example/bsef18a521/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.bsef18a521;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    public void openWebBrowser(View v){
+        Uri repo= Uri.parse("https://github.com/W-A-Q-A-R/bsef18a521-assignment-3");
+        Intent intent =new Intent(Intent.ACTION_VIEW, repo);
+        startActivity(intent);
     }
 }
